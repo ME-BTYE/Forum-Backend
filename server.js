e1ba1dc613036556085a0fbe546df0e921948d33
Your Name~ <you@example.com>
2023-11-15 20:10:12 -0800

    port env update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const userRouter = require("./server/api/users/user.router");
 const questionRouter = require("./server/api/questions1/question.router");
 const answerRouter = require("./server/api/Answers1/answer.router");
 const app = express();
-const port = process.env.PORT ;
+const port = process.env.PORT||80 ;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -16,4 +16,4 @@ app.use("/api/users", userRouter);
 app.use("/api/questions", questionRouter);
 app.use("/api/Answers", answerRouter);
 
-app.listen(port, () => console.log(`listening at http://localhost:${port}`));
+app.listen(port, "0.0.0.0", () => console.log(`listening at http://localhost:${port}`));
--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql2");
-
+require('dotenv').config()
 // const pool = mysql.createPool({
 //   host: process.env.DB.HOST,
 //   user: process.env.DB_USER,
